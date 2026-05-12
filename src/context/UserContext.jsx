4be0fb3f6b1e4019e1398dbabb40234f70bc6f32
tarequlislam332760-collab <tarequlislam332760@gmail.com>
2026-05-12 07:48:31 -0700

    Update UserContext.jsx

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -8,7 +8,7 @@ export const UserProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token'));
   const [loading, setLoading] = useState(true);
 
-  const API_URL = "https://vinance-backend.vercel.app"; 
+  const API_URL = "https://vinance-backend-1.onrender.com"; 
 
   // ✅ Login function logic fixed (Token first, then User)
   const login = (userToken, userData) => {
